b6b4de430edc52fd113fba964a513ae9ebaa2770
David O'Connell <oconned6@tcd.ie>
2025-12-01 21:34:55 +0000

    cooked

--- 1/main.py
+++ 1/main.py
@@ -11,9 +11,11 @@ cur = 50
 for line in lines:
     func = OPS[line[0]]
     val = int(line[1:])
-    cur = (cur + (func(100, val) % 100)) % 100
-    print(f"Line is {line}, OP is {func}, val is {val}")
-    if cur == 0:
+    total = func(cur, val)
+    ans = total%100
+    if ans == 0:
         res += 1
+    # res+= (total-ans)//100  # Cant figure part two
+    cur = ans
 
 print(res)
