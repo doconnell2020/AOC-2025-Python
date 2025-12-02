1793bbd4d40d01f8ffdc80a3ef3336429575472e
David O'Connell <oconned6@tcd.ie>
2025-12-02 18:26:07 +0000

    d2p1 solved

--- 2/main.py
+++ 2/main.py
@@ -6,26 +6,17 @@ with open(sys.argv[1]) as f:
 ids = []
 
 
-def length_filter(string: str):
+def part_one(string: str):
     a, b = string.split("-")
     if len(a) == len(b) and len(a) % 2 != 0:
         return
     else:
-        return string
+        even_ns = [str(i) for i in range(int(a), int(b) + 1) if len(str(i)) % 2 == 0]
+        valids = [int(n) for n in even_ns if n[: len(n) // 2] == n[len(n) // 2 :]]
+        ids.extend(valids)
 
 
-to_check = [length_filter(line) for line in lines]
+[part_one(line) for line in lines]
 
-
-def foo(string: str):
-    a, b = string.split("-")
-    even_ns = [str(i) for i in range(int(b[0]), int(b[1]) + 1) if len(str(i)) % 2 == 0]
-    print(even_ns)
-    valids = [n for n in even_ns if n[: len(n) // 2] == n[len(n) // 2 :]]
-    print(valids)
-
-
-foo("11-22")
-
-# print(lines)
-# assert short1 == 1227775554
+total = sum(ids)
+print(f"Part one is : {total}")
